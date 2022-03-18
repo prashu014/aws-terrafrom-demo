cdf04814a9ec690d22ae25d0fd160fb81a24efb5
prashant <prashant.nknsp@nic.in>
2022-03-18 15:27:35 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instancetype" {
+  default     = "t2.small"
+  description = "Category of EC2 instance"
+}
+
+# variable "instanceclass" {
+#   description = "Category of RDS instance"
+# }
